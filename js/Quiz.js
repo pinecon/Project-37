0539b77c72612f42d103dab38c4a6019a9a36e56
pinecon <83753612+pinecon@users.noreply.github.com>
2022-05-19 12:15:57 +0530

    Update Quiz.js

--- js/Quiz.js
+++ js/Quiz.js
@@ -63,19 +63,19 @@ class Quiz {
        //    fill("red");
        //  }
 
-        // if (correctAns === allContestants[plr].answer){
-        //   fill("red")
-        // }
-        // else{
-        //   fill("green");
-        // }
+         if (correctAns === allContestants[plr].answer){
+           fill("red")
+         }
+         else{
+           fill("green");
+         }
 
-        if (correctAns = allContestants[plr].answer){
-          fill("Green")
-        }
-        else{
-           fill("red");
-        }
+       // if (correctAns = allContestants[plr].answer){
+          //fill("Green")
+        //}
+        //else{
+      //     fill("red");
+       // }
 
         display_Answers+=30;
         textSize(20);
